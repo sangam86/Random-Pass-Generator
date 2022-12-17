fe176aa29fcc675f9b69c7cb19ebe00a58fa160d
sangam86 <109195823+sangam86@users.noreply.github.com>
2022-12-17 23:43:53 +0530

    Update TASK 1.py

--- TASK 1.py
+++ TASK 1.py
@@ -1,12 +1,11 @@
 
-# Python program to generate random
-# password using Tkinter module
+
 import random
 import pyperclip
 from tkinter import *
 from tkinter.ttk import *
 
-# Function for calculation of password
+
 
 
 def low():
@@ -17,22 +16,19 @@ def low():
 
 	lower = "abcdefghijklmnopqrstuvwxyz"
 	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
+	digits = "0123456789 !@#$%^&*()"
 	password = ""
 
-	# if strength selected is low
 	if var.get() == 1:
 		for i in range(0, length):
 			password = password + random.choice(lower)
 		return password
 
-	# if strength selected is medium
 	elif var.get() == 0:
 		for i in range(0, length):
 			password = password + random.choice(upper)
 		return password
 
-	# if strength selected is strong
 	elif var.get() == 3:
 		for i in range(0, length):
 			password = password + random.choice(digits)
@@ -41,30 +37,21 @@ def low():
 		print("Please choose an option")
 
 
-# Function for generation of password
 def generate():
 	password1 = low()
 	entry.insert(10, password1)
 
 
-# Function for copying password to clipboard
 def copy1():
 	random_password = entry.get()
 	pyperclip.copy(random_password)
 
 
-# Main Function
-
-# create GUI window
 root = Tk()
 var = IntVar()
 var1 = IntVar()
 
-# Title of your GUI window
 root.title("Random Password Generator")
-
-# create label and entry to show
-# password generated
 Random_password = Label(root, text="Password")
 Random_password.grid(row=0)
 entry = Entry(root)
@@ -74,17 +61,13 @@ entry.grid(row=0, column=1)
 c_label = Label(root, text="Length")
 c_label.grid(row=1)
 
-# create Buttons Copy which will copy
-# password to clipboard and Generate
-# which will generate the password
+
 copy_button = Button(root, text="Copy", command=copy1)
 copy_button.grid(row=0, column=2)
 generate_button = Button(root, text="Generate", command=generate)
 generate_button.grid(row=0, column=3)
 
-# Radio Buttons for deciding the
-# strength of password
-# Default strength is Medium
+
 radio_low = Radiobutton(root, text="Low", variable=var, value=1)
 radio_low.grid(row=1, column=2, sticky='E')
 radio_middle = Radiobutton(root, text="Medium", variable=var, value=0)
